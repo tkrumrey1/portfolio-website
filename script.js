14a1a8c06d844dae6be96fa4c444d0cb9b5824fe
tkrumrey1 <tkrumrey1@gmail.com>
2025-04-15 17:38:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
-  // This script can be used for any JS interactions if you decide to add more later.
   var dropdowns = document.querySelectorAll('.dropdown');
-  
+
   dropdowns.forEach(function(dropdown) {
     dropdown.addEventListener('mouseover', function(){
       var dropdownContent = this.querySelector('.dropdown-content');
